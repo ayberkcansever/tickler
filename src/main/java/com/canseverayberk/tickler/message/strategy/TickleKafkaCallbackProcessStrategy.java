856f5f01eedb17c2941f3895fb1c4d83ab0e372d
Ayberk Cansever <>
2021-01-16 14:44:58 +0300

    organize inputs

--- src/main/java/com/canseverayberk/tickler/message/strategy/TickleKafkaCallbackProcessStrategy.java
+++ src/main/java/com/canseverayberk/tickler/message/strategy/TickleKafkaCallbackProcessStrategy.java
@@ -1,8 +1,6 @@
 package com.canseverayberk.tickler.message.strategy;
 
 import com.canseverayberk.tickler.model.Tickle;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
