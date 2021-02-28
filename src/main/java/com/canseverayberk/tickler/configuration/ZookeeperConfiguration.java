e7070e2f1b71f0e43a01a84a43180de19aee3097
ayberk.cansever <ayberk.cansever@trendyol.com>
2021-02-28 10:13:49 +0300

    zookeeper disabled if redis background is not chosen

--- src/main/java/com/canseverayberk/tickler/configuration/ZookeeperConfiguration.java
+++ src/main/java/com/canseverayberk/tickler/configuration/ZookeeperConfiguration.java
@@ -2,6 +2,7 @@ package com.canseverayberk.tickler.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -13,6 +14,7 @@ import static com.canseverayberk.tickler.TicklerApplication.I_AM_LEADER;
 
 @Slf4j
 @Configuration
+@ConditionalOnBean(value = RedisConfiguration.class)
 public class ZookeeperConfiguration {
 
     @Bean
